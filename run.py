310f172e7b4195620f1a70caf1b15827ff91fac2
Emmanuel Chinda <97448054+GoodBoyC@users.noreply.github.com>
2022-03-25 09:55:24 +0100

    Add files via upload

--- run.py
+++ run.py
@@ -161,10 +161,26 @@ til ="•"
 
 ok, cp, id, user, pwx, loop = [], [], [], [], [], 0
 
+jarak = "     "
+ubahP = []
+pwbaru = []
+data = {}
+data2 = {}
+loop = 0
+loop = 0
+ok = []
+cp = []
+ttl = []
+id = []
+nampung = []
+data,data2={},{}
+ubahP,pwBaru=[],[]
+
 sys.stdout.write('\x1b[1;35m\x1b]2; {×} bff-2 by romz {×} \x07')
 
 #		crackmenu("").check_opsi()
 class Chinda():
+	
 	def buat_tgl(self):
 		global wak_
 		os.system("clear")
